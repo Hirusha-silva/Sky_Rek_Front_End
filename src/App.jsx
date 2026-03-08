d627e4d6effc9ba2cdc0653464d962a53b525c35
Hirusha-silva <hirushasilva222@gmail.com>
2026-03-08 22:04:24 +0530

    component use in another component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import ProductCard from './components/productCard'
+import Superproduct from './components/superProduct'
 
 function App() {
   return (
     <>
       <div>
+        <Superproduct/>
         <ProductCard name="Dell" price="$ 699" url="https://picsum.photos/id/2/200/300" />
         <ProductCard name="Acer" price="$ 899" url="https://picsum.photos/id/3/200/300" />
       </div>
---
+++ src/components/superProduct.jsx
@@ -0,0 +1,14 @@
+import ProductCard from "./productCard";
+
+export default function Superproduct(){
+    return(
+        <div>
+            <h1>Super Product !!!</h1>
+            <ProductCard        // me widihata use karanna puluwan
+                name="Iphone 16"
+                price="$ 300"
+                url="https://picsum.photos/id/20/200/300"
+            />
+        </div>
+    )
+}
