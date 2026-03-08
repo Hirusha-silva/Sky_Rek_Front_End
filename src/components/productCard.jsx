bc0add50f204369dfd217a28e33e2ada170cfe94
Hirusha-silva <hirushasilva222@gmail.com>
2026-03-08 20:17:58 +0530

    create own component

---
+++ src/components/productCard.jsx
@@ -0,0 +1,10 @@
+export default function ProductCard() {
+    return (
+        <div>
+            <h1>MacBook Air</h1>
+            <img src="https://picsum.photos/id/1/200/300"/>
+            <p>$999.99</p>
+            <button>View More</button>
+        </div>
+    )
+}
