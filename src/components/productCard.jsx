f0f73b8f0adc5ace91e4a3da507fb70c8c3ce8cb
Hirusha-silva <hirushasilva222@gmail.com>
2026-03-08 22:16:20 +0530

    add css

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,8 +1,10 @@
+import  "./productcard.css";
+
 export default function ProductCard(props) {
     console.log(props);
     
     return (
-        <div>
+        <div className="card">
             <h1>{props.name}</h1>
             <img src={props.url}/>
             <p>Price {props.price}</p>
