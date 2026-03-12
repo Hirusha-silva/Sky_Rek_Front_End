2f37f6c18e9fbc857f701485a15a6331cd321a71
Hirusha-silva <hirushasilva222@gmail.com>
2026-03-12 16:18:37 +0530

    create login page

--- src/pages/adminPage.jsx
+++ src/pages/adminPage.jsx
@@ -1,17 +1,20 @@
 import { Route, Routes, Link } from "react-router-dom";
 import { BsBoxSeamFill } from "react-icons/bs";
 import { GrInstall } from "react-icons/gr";
+import { BsPeopleFill } from "react-icons/bs";
+import { IoIosSettings } from "react-icons/io";
 
 export default function AdminPage() {
     return (
         <div className="w-full h-screen flex">
             <div className="w-[400px] h-full flex flex-col items-center">
                 <span className="text-3xl font-bold my-5">Admin Panel</span>
-                <Link className="flex  h-[60px] w-full justify-center items-center gap-3 text-3xl " to={"/admin/products"}><BsBoxSeamFill/>products</Link>
-                <Link className="flex  h-[60px] w-full justify-center items-center gap-3 text-3xl " to={"/admin/orders"}><GrInstall/>orders</Link>
-                <Link className="flex  h-[60px] w-full justify-center items-center gap-3 text-3xl " to={"/admin/user"}><BsBoxSeamFill/>User</Link>
+                <Link className="flex  h-[60px] w-full p-[20px]  border items-center gap-3 text-3xl " to={"/admin/products"}><BsBoxSeamFill/>products</Link>
+                <Link className="flex  h-[60px] w-full p-[20px] border items-center gap-3 text-3xl " to={"/admin/orders"}><GrInstall/>orders</Link>
+                <Link className="flex  h-[60px] w-full p-[20px] border items-center gap-3 text-3xl " to={"/admin/user"}><BsPeopleFill/>User</Link>
+                <Link className="flex  h-[60px] w-full p-[20px] border items-center gap-3 text-3xl " to={"/admin/settings"}><IoIosSettings/>Settings</Link>
             </div>
-            <div className="w-[calc(100%-400px)] h-full">
+            <div className="w-[calc(100%-400px)] h-full"> 
                 <Routes>
                     <Route path="/" element={<h1>Admin Dashboard</h1>} />
                     <Route path="/products" element={<h1>Products</h1>} />
--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
+
 export default function LoginPage() {
     return (
-        <div>
-            <span>Login Page</span>
+        <div className="w-full h-screen bg-[url(./backGround.jpg)] bg-cover bg-center flex justify-center items-center">
+            <div className="w-[600px] h-[600px] backdrop-blur-sm relative rounded-[60px] gap-[20px] shadow-2xl text-black flex flex-col items-center justify-center">
+                <h1 className="text-4xl absolute top-[20px] font-bold text-center my-5">Login</h1>
+                <div className=" w-[350px]  flex flex-col  ">
+                    <span className="text-lg">Email</span>
+                    <input type="text" className="w-[350px] border h-[50px] border-black rounded-3xl" />
+                </div>
+                <div className=" w-[350px]  flex flex-col  ">
+                    <span className="text-lg">Password</span>
+                    <input type="password" className="w-[350px] border h-[50px] border-black rounded-3xl" />
+                </div>
+                <button className="w-[350px] h-[50px] bg-blue-500 text-white rounded-3xl mt-5">Login</button>
+                <p className="text-lg mt-5">Don't have an account? <Link to={"/register"} className="text-blue-500">Sign up</Link></p>
+                
+            </div>
+            
+            
         </div>
     )
 }
