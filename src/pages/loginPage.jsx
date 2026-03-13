114fe9348a72b0206b3c1866b5574ef4589725c3
Hirusha-silva <hirushasilva222@gmail.com>
2026-03-14 01:25:23 +0530

    axios for send http request

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function LoginPage() {
     const[email,setEmail] = useState("")
@@ -9,32 +10,43 @@ export default function LoginPage() {
     function login(){
         console.log(email);
         console.log(password);
+
+        axios.post("http://localhost:5001/api/users/login",{
+            email:email,
+            password:password
+        }).then((res)=>{
+            console.log(res.data);
+        }).catch((err)=>{
+            console.log(err);
+        })
         
         
     }
 
     return (
         <div className="w-full h-screen bg-[url(./backGround.jpg)] bg-cover bg-center flex justify-center items-center">
+
             <div className="w-[600px] h-[600px] backdrop-blur-sm relative rounded-[60px] gap-[20px] shadow-2xl text-black flex flex-col items-center justify-center">
+
                 <h1 className="text-4xl absolute top-[20px] font-bold text-center my-5">Login</h1>
                 <div className=" w-[350px]  flex flex-col  ">
                     <span className="text-lg">Email</span>
                     <input onChange={(e)=>{
-                        setEmail(e.target.value)
+                        setEmail(e.target.value) //email eka state ekata set karanawa, e.target.value kiyanne input field eke thiyana value eka, e value eka email state ekata set karanawa
                     }} type="text" className="w-[350px] border h-[50px] border-black rounded-3xl" />
                 </div>
+
                 <div className=" w-[350px]  flex flex-col  ">
                     <span className="text-lg">Password</span>
                     <input onChange={(e)=>{
-                        setPassword(e.target.value)
+                        setPassword(e.target.value) //password eka state ekata set karanawa, e.target.value kiyanne input field eke thiyana value eka, e value eka password state ekata set karanawa
                     }}  type="password" className="w-[350px] border h-[50px] border-black rounded-3xl" />
                 </div>
+
                 <button onClick={login} className="w-[350px] h-[50px] bg-blue-500 text-white rounded-3xl mt-5">Login</button>
                 <p className="text-lg mt-5">Don't have an account? <Link to={"/register"} className="text-blue-500">Sign up</Link></p>
                 
             </div>
-            
-            
         </div>
     )
 }
