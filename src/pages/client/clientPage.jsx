4a4ef91885837b3a4d0a72cfaf8f8736cb1a1de3
Hirusha-silva <hirushasilva222@gmail.com>
2026-03-18 12:35:07 +0530

    create route in client page

--- src/pages/client/clientPage.jsx
+++ src/pages/client/clientPage.jsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "../../components/header";
 
 export default function ClientPage(){
@@ -5,6 +6,14 @@ export default function ClientPage(){
         <div className="w-full h-screen max-h-screen ">
             <Header/>
             <div className="w-full h-[calc(100%-100px)] bg-amber-400 ">
+                <Routes path="/">
+                    <Route path="/" element={<h1 className="text-3xl text-center">welcome to home page</h1>}></Route>
+                    <Route path="/products" element={<h1 className="text-3xl text-center"> products page</h1>}></Route>
+                    <Route path="/reviews" element={<h1 className="text-3xl text-center"> reviews page</h1>}></Route>
+                    <Route path="/about-us" element={<h1 className="text-3xl text-center"> about us page</h1>}></Route>
+                    <Route path="/contact-us" element={<h1 className="text-3xl text-center"> contact us page</h1>}></Route>
+                    <Route path="/*" element={<h1 className="text-3xl text-center">404 Not Found</h1>}></Route>
+                </Routes>
 
             </div>
         </div>
