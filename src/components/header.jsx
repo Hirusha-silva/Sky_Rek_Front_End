c18c9e63004dd85e35f68431b3c532bb4d0ca133
Hirusha-silva <hirushasilva222@gmail.com>
2026-03-22 12:36:04 +0530

    add cart icon in header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,15 @@
+import { BiCart } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
 export default function Header(){
     return(
-        <header className="h-[100px] bg-blue-600 flex justify-center items-center">
+        <header className="h-[100px] bg-blue-600 flex justify-center items-center relative">
             <Link to="/" className="ml-4 text-white text-xl ">Home</Link>
             <Link to="/products" className=" ml-4 text-white text-xl ">Products</Link>
             <Link to="/reviews" className=" ml-4 text-white text-xl ">Reviews</Link>
             <Link to="/about-us" className=" ml-4 text-white text-xl ">About Us</Link>
             <Link to="/contact-us" className=" ml-4 text-white text-xl ">Contact Us</Link>
+            <Link to="/cart" className="absolute right-[80px]  "><BiCart className="text-white text-3xl ml-4 "/> </Link>
         </header>
     ) 
     
--- src/pages/client/clientPage.jsx
+++ src/pages/client/clientPage.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Header from "../../components/header";
 import ProductPage from "./productPage";
 import OverViewPage from "./overViewPage";
+import CartPage from "./cart";
 
 export default function ClientPage(){
     return(
@@ -14,6 +15,7 @@ export default function ClientPage(){
                     <Route path="/reviews" element={<h1 className="text-3xl text-center"> reviews page</h1>}></Route>
                     <Route path="/about-us" element={<h1 className="text-3xl text-center"> about us page</h1>}></Route>
                     <Route path="/contact-us" element={<h1 className="text-3xl text-center"> contact us page</h1>}></Route>
+                    <Route path="/cart" element={<CartPage/>}></Route>
                     <Route path="/overView/:productId" element={<OverViewPage/>}></Route>
                     <Route path="/*" element={<h1 className="text-3xl text-center">404 Not Found</h1>}></Route>
                 </Routes>
