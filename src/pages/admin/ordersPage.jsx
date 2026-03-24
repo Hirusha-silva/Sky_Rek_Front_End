71d34efd66e93e1051a1135cbf00e74e775ef2bd
Hirusha-silva <hirushasilva222@gmail.com>
2026-03-24 14:49:07 +0530

    get orders from backend to frontend and show adming order page

--- src/pages/admin/ordersPage.jsx
+++ src/pages/admin/ordersPage.jsx
@@ -24,8 +24,39 @@ export default function OrdersPage(){
     },[loading])
 
     return (
-        <div className="w-full h-full flex">
-            <span>Order Page</span>
+        <div className="w-full h-full flex ">
+            <table className="w-full h-full border-[3px]">
+                <thead>
+                    <tr>
+                        <th className="p-[10px]">Order Id</th>
+                        <th className="p-[10px]">email</th>
+                        <th className="p-[10px]">name</th>
+                        <th className="p-[10px]">address</th>
+                        <th className="p-[10px]">phone</th>
+                        <th className="p-[10px]">status</th>
+                        <th className="p-[10px]">date</th>
+                        <th className="p-[10px]">total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        orders.map((order,index)=>{
+                            return(
+                            <tr key={order.orderId}>
+                                <td className="p-[10px]">{order.orderId}</td>
+                                <td className="p-[10px]">{order.email}</td>
+                                <td className="p-[10px]">{order.name}</td>
+                                <td className="p-[10px]">{order.address}</td>
+                                <td className="p-[10px]">{order.phone}</td>
+                                <td className="p-[10px]">{order.status}</td>
+                                <td className="p-[10px]">{new Date(order.date).toLocaleDateString()}</td>
+                                <td className="p-[10px] text-end">{order.total.toLocaleString('en-US', { style: 'currency', currency: 'LKR' })}</td>
+                            </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
         </div>
     )
 }
